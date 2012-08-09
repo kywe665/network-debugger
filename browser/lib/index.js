8b32aca84ee9f385fe785ef56a104eca2d162da8
tigerbot <sethg@spotter360.com>
2012-08-09 17:00:55 -0600

    moving around some functions and removing some unused variable to appease JSLint

--- browser/lib/index.js
+++ browser/lib/index.js
@@ -10,7 +10,6 @@
     , reqwest = require('reqwest')
     , window = require('window')
     , document = window.document
-    , location = window.location
     , uiTabs = require('./ui-tabs')
     , io = require('socket.io-browser')
     , socket
@@ -19,6 +18,74 @@
     , visual = require('./visual')
     , tabs = require('./newTab')
     ;
+
+  function processBody(options, data) {
+    var xml
+      , xml_pp
+      , json_pp
+      ;
+
+    if (!data) {
+      data = {};
+    }
+    if (!options || !options.hasOwnProperty('body')) {
+      console.error('options has no body:', JSON.stringify(options));
+      data.code += 'No Body';
+      return data;
+    }
+
+    //if xml
+    if (options.body.substring(0,3) === '<?x') {
+      xml_pp = pd.xml(options.body);
+      xml = xml_pp.replace(/&/g, '&amp;')
+                  .replace(/</g, '&lt;')
+                  .replace(/>/g, '&gt;');
+      data.xml = xml;
+    }
+    //if json
+    else if (options.body.charAt(0) === '{') {
+      json_pp = JSON.parse(options.body);
+      json_pp = JSON.stringify(json_pp, null, '  ');
+      json_pp = visual.syntaxHighlight(json_pp);
+      data.code += json_pp;
+    }
+    else {
+      data.code += options.body;
+    }
+    return data;
+  }
+
+  function scrollLock(options, port) {
+    var portName = port || options.protocol
+      , selector = '.js-ui-tab-view[data-name="'+portName+'"]'
+      ;
+    if ($(selector +' .js-scroll.js-'+options.protocol).attr('checked') && $(selector +' .js-'+options.protocol+'-stream')[0].scrollHeight !== 0) {
+      $(selector + ' .js-'+options.protocol+'-stream')[0].scrollTop = $(selector +' .js-'+options.protocol+'-stream')[0].scrollHeight;
+    }
+    if ($(selector +' .js-'+options.protocol+'-stream').children().length > 9) {
+      //console.log('cleared space: '+portName);
+      $(selector +' .js-'+options.protocol+'-stream span').first().remove();
+      $(selector +' .js-'+options.protocol+'-stream span').first().remove();
+    }
+  }
+
+  function preInjectCode(options) {
+    var data = {};
+    data.code = options.headers || '';
+    data = processBody(options, data);
+
+    pure.injectCode(options, data);
+    scrollLock(options, options.port);
+    visual.highlightMsg(options);
+  }
+
+  function injectMessage(options, port) {
+    pure.injectMessage(options.protocol, {
+      'message': options.body,
+      'class': options.cssClass
+    }, port);
+    scrollLock(options, port);
+  }
 
   function openListener(protocol, port, reopen) {
     var options = {}
@@ -33,6 +100,7 @@
     , type: 'json'
     , method: 'post'
     , error: function (err) {
+        console.error('Server Error: ', err);
         options.body = 'Cannot communicate with netbug server';
         options.cssClass = 'css-streamError';
         injectMessage(options);
@@ -83,9 +151,11 @@
     , method: 'put'
     , body: settings
     , error: function (err) {
+        console.error('Server Error: ', err);
         options.body = 'Cannot communicate with netbug server';
         options.cssClass = 'css-streamError';
-        injectMessage(options);
+        injectMessage(options, 'default');
+        injectMessage(options, options.port);
       }
     , success: function (resp) {
         // If there wasn't an error, we will get the event listenerChanged,
@@ -127,6 +197,7 @@
     , type: 'json'
     , method: 'del'
     , error: function (err) {
+        console.error('Server Error: ', err);
         options.body = 'Cannot communicate with netbug server';
         options.cssClass = 'css-streamError';
         injectMessage(options, 'default');
@@ -159,86 +230,54 @@
     });
   }
 
-  $(document).ready(function() {
-    var options = {};
-    options.protocol = 'all';
-    options.body = '';
-    openSocket(options);
-    uiTabs.create('body', '.js-ui-tab a', '.js-ui-tab', '.js-ui-tab-view', 'http/default');
-    reqwest({
-      url: 'http://'+window.location.host+'/onPageLoad'
-    , type: 'json'
-    , method: 'get'
-    , error: function (err) {
-        console.error('Server Error: ', err);
-        options.body = 'Cannot communicate with netbug server';
-        options.cssClass = 'css-streamError';
-        injectMessage(options);
-      }
-    , success: function (resp) {
-        var html, i;
-        if(!resp.error){
-          initBuild(resp);
-        }
-        else{
-          options.cssClass = 'css-streamError';
-          for(i=0; i < resp.errors.length; i=i+1){
-						options.body += resp.errors[i].message;
-					}
-          injectMessage(options);
-        }
-      }
-    });
-  });
-
   //EVENT LISTENERS ALL
-  $('.container').on('.js-all-stream pre', 'click', function(){
+  $('.container').on('.js-all-stream pre', 'click', function () {
     $(this).toggleClass('css-hl-block');
   });
-  $('.container').on('.js-ui-tab-view:not(.css-active) .js-openSocket', 'click', function(){
+  $('.container').on('.js-ui-tab-view:not(.css-active) .js-openSocket', 'click', function () {
     var protocol = $(this).attr('data-protocol')
       , port = $('.js-portNum.js-'+protocol).val()
       ;
-    if(!port){
+    if (!port) {
       return;
     }
     openListener(protocol, port);
 	});
-  $('.container').on('.js-ui-tab-view:not(.css-active) .js-reopen', 'click', function(){
+  $('.container').on('.js-ui-tab-view:not(.css-active) .js-reopen', 'click', function () {
     openListener($(this).attr('data-protocol'), $(this).attr('data-port'), true);
 	});
-  $('.container').on('.js-ui-tab-view .js-close-tab', 'click', function(){
+  $('.container').on('.js-ui-tab-view .js-close-tab', 'click', function () {
    var protocol = $(this).parent().attr('data-protocol')
       , port = $(this).parent().find('a').html()
       ;
     socket.emit('kill' + protocol, port);
     tabs.closeTab(port, this);
 	});
-  $('.container').on('.js-ui-tab-view:not(.css-active) .js-portNum', 'keypress', function(e){
-    if(e.keyCode === 13){
+  $('.container').on('.js-ui-tab-view:not(.css-active) .js-portNum', 'keypress', function (e) {
+    if (e.keyCode === 13) {
       $('.js-openSocket.js-'+$(this).attr('data-protocol')).trigger('click');
     }
   });
-  $('.container').on('.js-scroll', 'change', function(){
+  $('.container').on('.js-scroll', 'change', function () {
     scrollLock({
       protocol: $(this).attr('data-protocol')
     }, $(this).closest('.js-ui-tab-view').attr('data-name'));
   });
-  $('.container').on('.js-clear', 'click', function(){
+  $('.container').on('.js-clear', 'click', function () {
     $(this).closest('.js-ui-tab-view').find('.js-'+$(this).attr('data-protocol')+'-stream').html('');
   });
-  $('.container').on('.js-ui-tab-view:not(.css-inactive) .js-log', 'click', function(){
-    if($(this).attr('data-protocol') === 'http'){
+  $('.container').on('.js-ui-tab-view:not(.css-inactive) .js-log', 'click', function () {
+    if ($(this).attr('data-protocol') === 'http') {
       var port = $(this).closest('.js-ui-tab-view').attr('data-name');
       socket.emit('log' + $(this).attr('data-protocol'), port);
       $(this).toggleClass('activeLog');
     }
-    else{
+    else {
       socket.emit('log' + $(this).attr('data-protocol'));
       $(this).toggleClass('activeLog');
     }
   });
-  $('.container').on('.js-ui-tab-view:not(.css-inactive) .js-closeSocket', 'click', function() {
+  $('.container').on('.js-ui-tab-view:not(.css-inactive) .js-closeSocket', 'click', function () {
     var protocol = $(this).attr('data-protocol')
       , port = $(this).closest('.js-ui-tab-view').attr('data-name')
       ;
@@ -249,7 +288,7 @@
   });
 
   //EVENT LISTENERS HTTP
-  $('.container').on('.js-include-headers', 'change', function(){
+  $('.container').on('.js-include-headers', 'change', function () {
     socket.emit('includeHeaders', $('.js-include-headers').attr('checked'));
   });
 
@@ -277,7 +316,7 @@
         console.log('TODO: implement listenerChanged:', msg.protocol, msg.port, msg.logSettings);
       });
 
-      socket.on('listenerClosed', function(msg) {
+      socket.on('listenerClosed', function (msg) {
         var options = {};
 
         options.body = msg.protocol.toUpperCase() + ' Listener on port ' + msg.port + ' closed';
@@ -307,7 +346,7 @@
 
   function initBuild(resp) {
     var options = {};
-    options.body = '';
+
     Object.keys(resp.result).forEach(function (protocol) {
       if (resp.result[protocol].length > 0) {
         resp.result[protocol].forEach(function (listener) {
@@ -321,73 +360,45 @@
         });
       }
     });
+
+    openSocket();
   }
 
-  function scrollLock(options, port) {
-    var portName = port || options.protocol
-      , selector = '.js-ui-tab-view[data-name="'+portName+'"]'
-      ;
-    if($(selector +' .js-scroll.js-'+options.protocol).attr('checked') && $(selector +' .js-'+options.protocol+'-stream')[0].scrollHeight !== 0){
-      $(selector + ' .js-'+options.protocol+'-stream')[0].scrollTop = $(selector +' .js-'+options.protocol+'-stream')[0].scrollHeight;
-    }
-    if($(selector +' .js-'+options.protocol+'-stream').children().length > 9){
-      //console.log('cleared space: '+portName);
-      $(selector +' .js-'+options.protocol+'-stream span').first().remove();
-      $(selector +' .js-'+options.protocol+'-stream span').first().remove();
-    }
-  }
+  $(document).ready(function () {
+    var options = {};
+    options.protocol = 'all';
+    options.body = '';
 
-  function injectMessage(options, port) {
-    pure.injectMessage(options.protocol, {
-      'message': options.body,
-      'class': options.cssClass
-    }, port);
-    scrollLock(options, port);
-  }
+    uiTabs.create('body', '.js-ui-tab a', '.js-ui-tab', '.js-ui-tab-view', 'http/default');
 
-  function preInjectCode(options) {
-    var data = {};
-    data.code = options.headers || '';
-    data = processBody(options, data);
+    reqwest({
+      url: 'http://'+window.location.host+'/onPageLoad'
+    , type: 'json'
+    , method: 'get'
+    , error: function (err) {
+        console.error('Server Error: ', err);
+        options.body = 'Cannot communicate with netbug server';
+        options.cssClass = 'css-streamError';
+        injectMessage(options);
+      }
+    , success: function (resp) {
+        if (!resp.error) {
+          initBuild(resp);
+        }
+        else {
+          options.cssClass = 'css-streamError';
+          if (Array.isArray(resp.errors)) {
+            resp.errors.forEach(function (error) {
+              options.body += error.message;
+            });
+          }
+          if (!options.body) {
+            options.body = 'Unknown error on page load';
+          }
+          injectMessage(options);
+        }
+      }
+    });
+  });
 
-    pure.injectCode(options, data);
-    scrollLock(options, options.port);
-    visual.highlightMsg(options);
-  }
-
-  function processBody(options, data) {
-    var xml
-      , xml_pp
-      , json_pp
-      ;
-
-    if (!data) {
-      data = {};
-    }
-    if (!options || !options.hasOwnProperty('body')) {
-      console.error('options has no body:', JSON.stringify(options));
-      data.code += 'No Body';
-      return data;
-    }
-
-    //if xml
-    if(options.body.substring(0,3) === '<?x'){
-      xml_pp = pd.xml(options.body);
-      xml = xml_pp.replace(/&/g, '&amp;')
-                  .replace(/</g, '&lt;')
-                  .replace(/>/g, '&gt;');
-      data.xml = xml;
-    }
-    //if json
-    else if(options.body.charAt(0) === '{'){
-      json_pp = JSON.parse(options.body);
-      json_pp = JSON.stringify(json_pp, null, '  ');
-      json_pp = visual.syntaxHighlight(json_pp);
-      data.code += json_pp;
-    }
-    else{
-      data.code += options.body;
-    }
-    return data;
-  }
 }());
